5c87749fd35303ac11e93181b483554ec90c74ee
Himanshu <himanshudalal720@gmail.com>
2026-01-19 15:47:12 +0530

    First commit

---
+++ Qustion1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static bool IsVowel(char c)
+    {
+        return "aeiou".Contains(char.ToLower(c));
+    }
+
+    static void Main()
+    {
+        string first = Console.ReadLine();
+        string second = Console.ReadLine();
+
+        var secondSet = second.ToLower().ToHashSet();
+
+        var filtered = first
+            .Where(c => IsVowel(c) || !secondSet.Contains(char.ToLower(c)))
+            .ToList();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < filtered.Count; i++)
+        {
+            if (i == 0 || filtered[i] != filtered[i - 1])
+                result.Append(filtered[i]);
+        }
+
+        console.WriteLine(result.ToString());
+    }
+}
---
+++ Qustion4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Marks { get; set; }
+}
+class StudentComparer : IComparer<Student>
+{
+    public int Compare(Student x, Student y)
+    {
+        int marksCompare = y.Marks.CompareTo(x.Marks);
+        if (marksCompare != 0)
+            return marksCompare;
+
+        return x.Age.CompareTo(y.Age);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        
+    }
+}
